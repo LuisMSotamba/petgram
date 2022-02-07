ec12747735c02559f12a4ee5623ecb57d3f18644
Luis Miguel Sotamba <luis.sotamba@ucuenca.edu.ec>
2022-02-06 22:51:35 -0500

    style the categoty component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Anchor, Image } from './styles'
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpeg'
 
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <a href={path}>
-    <img src={cover} />
+  <Anchor href={path}>
+    <Image src={cover} />
     {emoji}
-  </a>
+  </Anchor>
 )
